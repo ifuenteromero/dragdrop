039760086021ab465cd6b8da000acec5ad1e2664
Irene Fuente Romero <irene.fuente@ultimasoftware.com>
2021-03-17 09:55:59 +0100

    varios grupos

--- src/App.js
+++ src/App.js
@@ -16,7 +16,25 @@ const App = () => {
                 <p>ITEM 2</p>
               </div>
             </div>
+            <div className="dnd-item">
+              <div>
+                <p>ITEM 3</p>
+              </div>
+            </div>
           </div>
+          <div className="dnd-group">
+            <div className="dnd-item">
+              <div>
+                <p>ITEM 4</p>
+              </div>
+            </div>
+            <div className="dnd-item">
+              <div>
+                <p>ITEM 5</p>
+              </div>
+            </div>
+          </div>
+          <div className="dnd-group"></div>
         </div>
       </header>
     </div>
