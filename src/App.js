aec7be09826dbe17b4821bba426b501ef1b9add7
Irene Fuente Romero <irene.fuente@ultimasoftware.com>
2021-03-17 10:21:29 +0100

    array de datos

--- src/App.js
+++ src/App.js
@@ -1,40 +1,26 @@
 import "./App.css";
 
 const App = () => {
+  const data = [
+    { title: "Group 1", items: [1, 2, 3] },
+    { title: "Group 2", items: [4, 5] },
+  ];
   return (
     <div className="App">
       <header className="App-header">
         <div className="drag-n-drop">
-          <div className="dnd-group">
-            <div className="dnd-item">
-              <div>
-                <p>ITEM 1</p>
-              </div>
+          {data.map((group, groupI) => (
+            <div key={groupI} className="dnd-group">
+              <div className="group-title">{group.title}</div>
+              {group.items.map((item, itemI) => (
+                <div key={itemI} className="dnd-item">
+                  <div>
+                    <p>{`ITEM ${item}`}</p>
+                  </div>
+                </div>
+              ))}
             </div>
-            <div className="dnd-item">
-              <div>
-                <p>ITEM 2</p>
-              </div>
-            </div>
-            <div className="dnd-item">
-              <div>
-                <p>ITEM 3</p>
-              </div>
-            </div>
-          </div>
-          <div className="dnd-group">
-            <div className="dnd-item">
-              <div>
-                <p>ITEM 4</p>
-              </div>
-            </div>
-            <div className="dnd-item">
-              <div>
-                <p>ITEM 5</p>
-              </div>
-            </div>
-          </div>
-          <div className="dnd-group"></div>
+          ))}
         </div>
       </header>
     </div>
