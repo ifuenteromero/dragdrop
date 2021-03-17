ee5da435a7199185aaba321f9ef4d97178e965e2
Irene Fuente Romero <irene.fuente@ultimasoftware.com>
2021-03-17 09:28:31 +0100

    añado estilos a una card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">hola</div>;
-}
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="drag-n-drop">
+          <div className="dnd-group">
+            <div className="dnd-item">
+              <div>
+                <p>ITEM 1</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
 
 export default App;
